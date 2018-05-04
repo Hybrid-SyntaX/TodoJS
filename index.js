0aea1d499ba0eb8e1f0e076b4160c19fd42b8ac1
Abbas <hybrid_syntax@hotmail.com>
2018-05-04 12:17:33 +0430

    add index

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log(process.env.NODE_ENV);
+const express = require("express");
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up production assets
+  //Like our main.js file, or main.css file
+  app.use(express.static("client/build"));
+
+  //Express will serve up the index.html file
+  //If it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
